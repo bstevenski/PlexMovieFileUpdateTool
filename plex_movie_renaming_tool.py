630aa60f0c0d98739b3fe2f9e2d32b3df5f69106
bristevenski <45806295+bristevenski@users.noreply.github.com>
2021-02-01 18:59:40 -0600

    Added more filename checks
    Updated to add more formatting and updating to filenames to increase likelihood of finding a match, added check for invalid chars for filenames when renaming

--- plex_movie_renaming_tool.py
+++ plex_movie_renaming_tool.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python
-import imdb, os, difflib
+import imdb, os, difflib, re
 
 def find_searchable_name(file):
+    #remove extension
     file = os.path.splitext(file)[0]
-    file = file.lower().split("title")[0]
-    file = file.replace("_", " ")
     
-    return file
+    #remove common addition from ripping DVDs
+    file = file.lower().split("title")[0]
+    
+    #replace any common delimiters within filename to be spaces
+    common_delimiters = ['_', '.']
+    for char in common_delimiters:
+        file = file.replace(char, ' ')
+    
+    return file.strip()
 
 def change_filename(path, original_file, movie):
     try:
         movie_title = movie['title']
         movie_id = movie.movieID
         movie_year = movie['year']
+        
+        #remove any invalid chars from titles
+        invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+        for char in invalid_chars:
+            movie_title = movie_title.replace(char, '')
+        
+        #get filenames ready
         new_folder = "{title} ({year}) {{imdb-tt{id}}}".format(title = movie_title, year = movie_year, id = movie_id)
         new_filename = "{movie_folder}{ext}".format(movie_folder = new_folder, ext = os.path.splitext(original_file)[1])
         new_file = os.path.join(path, new_folder, new_filename)
         original_file = os.path.join(path, original_file)
         
+        #create new folder and move file
         os.mkdir(os.path.join(path, new_folder))
         os.rename(original_file, new_file)
+        
         print("Renamed {} to {}.".format(original_file, new_file))
     
     except Exception as e:
